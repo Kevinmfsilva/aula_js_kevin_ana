00fe33de29a418b8f12b434f0bb569d2cee02afb
Kevinmfsilva <166409636+Kevinmfsilva@users.noreply.github.com>
2024-10-07 09:00:51 -0300

    Add files via upload

---
+++ AULA05_PRATICA_IMC_ANA_KEVIN/script.js
@@ -0,0 +1,27 @@
+var peso;
+var altura;
+var imc;
+var resultado;
+
+//1 recebendo o event
+function calcular(event){
+    //refresh dos inputs
+
+    //event.preventDefault();
+    //alert('Teste');
+
+    //recebendo os dados digitados pelo usúario
+    peso = document.getElementById('peso').value;
+    altura = document.getElementById('altura').value;
+
+    //calculo imc
+    imc = peso/(altura*altura);
+
+    //testando a recepsao dos valores peso e altura
+    //e testando o calculo imc
+    console.log(peso);
+    console.log(altura);
+    console.log(imc);
+    
+
+}
